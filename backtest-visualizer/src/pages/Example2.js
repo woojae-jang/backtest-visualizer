ea7f969af3f6d1da0cfdca52d10a46f1d12e97eb
woo jae <dnwogo@naver.com>
2019-02-20 10:53:54 +0900

    add 'Programmatically navigate' example

--- backtest-visualizer/src/pages/Example2.js
+++ backtest-visualizer/src/pages/Example2.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 class Example2 extends React.Component {
+  onSubmit = () => {
+    this.props.history.push("/");
+  };
+
   render() {
-    return <h1>Example2</h1>;
+    return (
+      <form>
+        <input placeholder="name" type="name" />
+        <input placeholder="email" type="email" />
+        <button onClick={this.onSubmit}>Submit</button>
+      </form>
+    );
   }
 }
 export default Example2;
