e977d70dd37f98a3a466814e6a5458c05ab7ffbd
woo <dnwogo@naver.com>
2019-03-11 23:54:23 +0900

    create example chart

--- backtest-visualizer/src/index.js
+++ backtest-visualizer/src/index.js
@@ -11,7 +11,7 @@ import {
 import Example1 from "./pages/Example1";
 import Example2 from "./pages/Example2";
 import Example3s from "./pages/Example3";
-import Example4 from "./pages/Example4";
+import AssetAllocationChart from "./pages/Example4";
 import Notfound from "./pages/notfound";
 
 const routing = (
@@ -49,7 +49,7 @@ const routing = (
         <Route path="/example1" component={Example1} />
         <Route path="/example2" component={Example2} />
         <Route path="/example3" component={Example3s} />
-        <Route path="/example4" component={Example4} />
+        <Route path="/example4" component={AssetAllocationChart} />
         <Route component={Notfound} />
       </Switch>
     </div>
--- backtest-visualizer/src/pages/Example4.js
+++ backtest-visualizer/src/pages/Example4.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
+import Chart from "chart.js";
 
-class Example4 extends React.Component {
+class AssetAllocationChart extends Component {
   render() {
-    return <h1>Example4</h1>;
+    return (
+      <div id="chart">
+        chart.js chart exam
+        <canvas id="scatter-chart" width="800" height="450" />
+      </div>
+    );
+  }
+  componentDidMount() {
+    this._create_chart();
+  }
+
+  _create_chart() {
+    this.chart = new Chart(document.getElementById("scatter-chart"), {
+      type: "scatter",
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: "Scatter Dataset",
+            data: [{ x: 10, y: 20 }, { x: 15, y: 0 }],
+            backgroundColor: "rgb(255, 99, 132)"
+          }
+        ]
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              type: "linear",
+              position: "bottom"
+            }
+          ]
+        }
+      }
+    });
   }
 }
-export default Example4;
+
+export default AssetAllocationChart;
