bb451ffdbd9bbd7854ec21735794334b7fb82358
woo <dnwogo@naver.com>
2019-03-11 23:42:28 +0900

    add example4 page

--- backtest-visualizer/src/index.js
+++ backtest-visualizer/src/index.js
@@ -11,6 +11,7 @@ import {
 import Example1 from "./pages/Example1";
 import Example2 from "./pages/Example2";
 import Example3s from "./pages/Example3";
+import Example4 from "./pages/Example4";
 import Notfound from "./pages/notfound";
 
 const routing = (
@@ -37,12 +38,18 @@ const routing = (
             Example3
           </NavLink>
         </li>
+        <li>
+          <NavLink activeClassName="active" to="/example4">
+            Example4
+          </NavLink>
+        </li>
       </ul>
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/example1" component={Example1} />
         <Route path="/example2" component={Example2} />
         <Route path="/example3" component={Example3s} />
+        <Route path="/example4" component={Example4} />
         <Route component={Notfound} />
       </Switch>
     </div>
---
+++ backtest-visualizer/src/pages/Example4.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Example4 extends React.Component {
+  render() {
+    return <h1>Example4</h1>;
+  }
+}
+export default Example4;
