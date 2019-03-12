2f7e2b14baa542e5c444b5e42c267a2cc28d9a7d
woo jae <dnwogo@naver.com>
2019-03-12 23:58:20 +0900

    DatePicker to RangePicker

--- backtest-visualizer/src/pages/Example5.js
+++ backtest-visualizer/src/pages/Example5.js
@@ -2,12 +2,18 @@ import React from "react";
 import { DatePicker } from "antd";
 import "antd/dist/antd.css";
 
+const { RangePicker } = DatePicker;
+
+function onChange(date, dateString) {
+  console.log(date, dateString);
+}
+
 class Example5 extends React.Component {
   render() {
     return (
       <div>
         <h1>Example5</h1>
-        <DatePicker />
+        <RangePicker onChange={onChange} />
       </div>
     );
   }
