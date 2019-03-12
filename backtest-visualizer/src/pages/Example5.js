57b876ae4a3986495ba0e5236610902d1e283c0e
woo jae <dnwogo@naver.com>
2019-03-12 23:50:36 +0900

    example5 antd DatePicker

--- backtest-visualizer/src/pages/Example5.js
+++ backtest-visualizer/src/pages/Example5.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { DatePicker } from "antd";
+import "antd/dist/antd.css";
 
 class Example5 extends React.Component {
   render() {
-    return <h1>Example5</h1>;
+    return (
+      <div>
+        <h1>Example5</h1>
+        <DatePicker />
+      </div>
+    );
   }
 }
 export default Example5;
