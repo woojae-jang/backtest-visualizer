821561d494ff7ceeb2553b92e03ad0e1e086a933
woo jae <dnwogo@naver.com>
2019-03-17 22:58:37 +0900

    add loop method to AssetAllocationPage

--- backtest-visualizer/src/pages/AssetAllocationPage.js
+++ backtest-visualizer/src/pages/AssetAllocationPage.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { BackTest, BackTestArgsHandler } from "../utils/simulation";
 import { getRandomAllocation } from "../utils/utils";
-import * as mathjs from "mathjs";
-// import ChartExample5 from '../chart5/ChartExample5';
-// import DateRange from '../input/DateRange';
 import AssetAllocationChart from "../components/AssetAllocationChart";
+import MarketCalendar from "../components/MarketCalendar";
+import { GET_GLOBAL_VARIABLES } from "../apollo/queries";
+import { Query } from "react-apollo";
 
 class AssetAllocationPage extends Component {
   // state = {
@@ -49,14 +49,15 @@ class AssetAllocationPage extends Component {
   }
 
   simulationLoop() {
-    let i = 0;
+    // let i = 0;
     const startTime = Date.now();
     let curTime = Date.now();
-    let bestAllocation = null;
+    // let bestAllocation = null;
     while (true) {
       // if(i == 5000) break;
 
-      let resultObject = this.simulationOnce();
+      this.simulationOnce();
+      // let resultObject = this.simulationOnce();
       // if(bestAllocation === null){
       // bestAllocation = resultObject;
       // } else {
@@ -68,10 +69,29 @@ class AssetAllocationPage extends Component {
       // }
       // }
 
-      i++;
+      // i++;
 
       curTime = Date.now();
       if (curTime - startTime > 1000) break;
+    }
+  }
+
+  simulationLoopWithTimeLimit(limitTime = 1000) {
+    const startTime = Date.now();
+    let curTime = Date.now();
+    while (true) {
+      this.simulationOnce();
+      curTime = Date.now();
+      if (curTime - startTime > limitTime) break;
+    }
+  }
+
+  simulationLoopWithCountsLimit(limitCounts = 1000) {
+    let i = 0;
+    while (true) {
+      if (i === limitCounts) break;
+      this.simulationOnce();
+      i++;
     }
   }
 
@@ -89,17 +109,22 @@ class AssetAllocationPage extends Component {
     });
   }
 
-  // <div>{this.props.slidersBundle}</div>
   render() {
     return (
-      <div className="asset-allocation-page">
-        {/* <div>{this.props.datePickers}</div> */}
-        {/* <div>{this.props.datePickers}</div> */}
-        {/* <DateRange /> */}
-        <button onClick={this.handleOnClick}>Run</button>
-        <button onClick={this.handleResetClick}>Reset</button>
-        <AssetAllocationChart data={this.state.data} />
-      </div>
+      <Query query={GET_GLOBAL_VARIABLES}>
+        {({ loading, error, data, client }) => {
+          return (
+            <div>
+              <div className="asset-allocation-page">
+                <MarketCalendar data={data} client={client} />
+                <button onClick={this.handleOnClick}>Run</button>
+                <button onClick={this.handleResetClick}>Reset</button>
+                <AssetAllocationChart data={this.state.data} />
+              </div>
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
