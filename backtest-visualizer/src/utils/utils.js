6c997118ff86ab6529005082f165a1f3fea6ba4c
woo jae <dnwogo@naver.com>
2019-03-22 22:27:05 +0900

    add getRandAllocWithFixedWeights method

--- backtest-visualizer/src/utils/utils.js
+++ backtest-visualizer/src/utils/utils.js
@@ -12,4 +12,24 @@ const getRandomAllocation = division => {
   return weight_list;
 };
 
-export { getRandomAllocation };
+const getRandAllocWithFixedWeights = weightsWithfixedWeight => {
+  const numOfFreeAssets = weightsWithfixedWeight.filter(d => d === null).length;
+  const weightsOfFree =
+    100 - mathjs.sum(weightsWithfixedWeight.filter(d => d !== null));
+
+  let weightsOfFreeAssets = getRandomAllocation(numOfFreeAssets);
+
+  const sumOfWeights = mathjs.sum(weightsOfFreeAssets);
+  weightsOfFreeAssets = mathjs.multiply(
+    weightsOfFreeAssets,
+    weightsOfFree / sumOfWeights
+  );
+
+  const output = weightsWithfixedWeight.map(d => {
+    if (d !== null) return d;
+    return weightsOfFreeAssets.shift();
+  });
+  return output;
+};
+
+export { getRandomAllocation, getRandAllocWithFixedWeights };
