0631cbcf2f3391d78b9a3c748fa49c04f6ccddf2
woo jae <dnwogo@naver.com>
2019-03-23 23:59:57 +0900

    .

--- backtest-visualizer/src/components/RiskChart.js
+++ backtest-visualizer/src/components/RiskChart.js
@@ -44,6 +44,8 @@ class RiskChart extends React.Component {
     const datasets = [];
     const daysList = windowList;
     const colors = this.chart.data.datasets.map(dataset => dataset.borderColor);
+    const hidden = this.chart.data.datasets.map(dataset => dataset.hidden);
+    console.log(hidden);
     daysList.forEach((days, index) => {
       const newColor = dynamicColors();
       let data = getStdMovingAvg(pctChange, days);
@@ -55,7 +57,8 @@ class RiskChart extends React.Component {
         backgroundColor: colors[index] ? colors[index] : newColor,
         borderColor: colors[index] ? colors[index] : newColor,
         data: data,
-        fill: false
+        fill: false,
+        hidden: hidden[index]
       };
       datasets.push(dataset);
     });
