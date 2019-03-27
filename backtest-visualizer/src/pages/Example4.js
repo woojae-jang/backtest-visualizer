efcae07e4feb396078525474b6b45cbd417e07ac
woo jae <dnwogo@naver.com>
2019-03-27 23:09:41 +0900

    .

--- backtest-visualizer/src/pages/Example4.js
+++ backtest-visualizer/src/pages/Example4.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import ResultTable from "../components/ResultTable";
-import WeightsInputTable from "../components/WeightsInputTable";
+import WeightsInputTable from "components/WeightsInputTable";
 
 class AssetAllocationChart extends Component {
   render() {
     return (
       <div>
-        <ResultTable />
         <WeightsInputTable />
       </div>
     );
