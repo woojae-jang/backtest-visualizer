603d0c2603a9b9dae7b10fa8fdd8a3e874c7e0a5
woo jae <dnwogo@naver.com>
2019-04-28 23:51:52 +0900

    .

--- backtest-visualizer/src/pages/CorrelationMatrix/CorrelationMatrix.js
+++ backtest-visualizer/src/pages/CorrelationMatrix/CorrelationMatrix.js
@@ -3,7 +3,7 @@ import "./index.css";
 import * as d3 from "d3";
 import { Query } from "react-apollo";
 import * as jStat from "jStat";
-import { GET_GLOBAL_VARIABLES, GET_COR_PAGE } from "apollo/queries";
+import { GET_GLOBAL_VARIABLES } from "apollo/queries";
 import { Market } from "market";
 
 const market = new Market();
@@ -21,170 +21,193 @@ class CorrelationMatrix extends React.Component {
   componentDidMount() {
     d3.csv(
       "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_correlogram.csv"
-    ).then(function(_data) {
-      var data = [];
-
-      console.log(_data);
-      let rows = _data;
-
-      rows.forEach(function(d) {
-        var x = d[""];
-        delete d[""];
-        for (let prop in d) {
-          var y = prop,
-            value = d[prop];
-          data.push({
-            x: x,
-            y: y,
-            value: +value
-          });
-        }
-      });
-
-      var margin = {
-          top: 25,
-          right: 80,
-          bottom: 25,
-          left: 25
-        },
-        width = 500 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom,
-        domain = d3
-          .set(
-            data.map(function(d) {
-              return d.x;
-            })
-          )
-          .values(),
-        num = Math.sqrt(data.length),
-        color = d3
-          .scaleLinear()
-          .domain([-1, 0, 1])
-          .range(["#B22222", "#fff", "#000080"]);
-
-      var x = d3
-          .scalePoint()
-          .range([0, width])
-          .domain(domain),
-        y = d3
-          .scalePoint()
-          .range([0, height])
-          .domain(domain),
-        xSpace = x.range()[1] - x.range()[0],
-        ySpace = y.range()[1] - y.range()[0];
-      ySpace = y.range()[1] - y.range()[0];
-
-      var svg = d3
-        .select("#matrix")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      var cor = svg
-        .selectAll(".cor")
-        .data(data)
-        .enter()
-        .append("g")
-        .attr("class", "cor")
-        .attr("transform", function(d) {
-          return "translate(" + x(d.x) + "," + y(d.y) + ")";
-        });
-
-      // it fits with v3
-      // cor.append("rect")
-      //   .attr("width", xSpace)
-      //   .attr("height", ySpace)
-      //   .attr("x", -xSpace / 2)
-      //   .attr("y", -ySpace / 2)
-
-      //edited to fit with v4 update  2/5/18
-      cor
-        .append("rect")
-        .attr("width", xSpace / 10)
-        .attr("height", ySpace / 10)
-        .attr("x", -xSpace / 20)
-        .attr("y", -ySpace / 20);
-
-      cor
-        .filter(function(d) {
-          var ypos = domain.indexOf(d.y);
-          var xpos = domain.indexOf(d.x);
-          for (var i = ypos + 1; i < num; i++) {
-            if (i === xpos) return false;
-          }
-          return true;
-        })
-        .append("text")
-        .attr("y", 5)
-        .text(function(d) {
-          if (d.x === d.y) {
-            return d.x;
-          } else {
-            return d.value.toFixed(2);
-          }
-        })
-        .style("fill", function(d) {
-          if (d.value === 1) {
-            return "#000";
-          } else {
-            return color(d.value);
-          }
-        });
-
-      cor
-        .filter(function(d) {
-          var ypos = domain.indexOf(d.y);
-          var xpos = domain.indexOf(d.x);
-          for (var i = ypos + 1; i < num; i++) {
-            if (i === xpos) return true;
-          }
-          return false;
-        })
-        .append("circle")
-        .attr("r", function(d) {
-          return (width / (num * 2)) * (Math.abs(d.value) + 0.1);
-        })
-        .style("fill", function(d) {
-          if (d.value === 1) {
-            return "#000";
-          } else {
-            return color(d.value);
-          }
-        });
-
-      var aS = d3
-        .scaleLinear()
-        .range([-margin.top + 5, height + margin.bottom - 5])
-        .domain([1, -1]);
-
-      var yA = d3
-        .axisRight()
-        .scale(aS)
-        .tickPadding(7);
-
-      var aG = svg
-        .append("g")
-        .attr("class", "y axis")
-        .call(yA)
-        .attr("transform", "translate(" + (width + margin.right / 2) + " ,0)");
-
-      var iR = d3.range(-1, 1.01, 0.01);
-      var h = height / iR.length + 3;
-      iR.forEach(function(d) {
-        aG.append("rect")
-          .style("fill", color(d))
-          .style("stroke-width", 0)
-          .style("stoke", "none")
-          .attr("height", h)
-          .attr("width", 10)
-          .attr("x", 0)
-          .attr("y", aS(d));
-      });
-    });
+    ).then(generateCorMatrix);
   }
 }
+
+const generateCorMatrix = _data => {
+  var data = [];
+
+  console.log(_data);
+  let rows = _data;
+
+  rows.forEach(function(d) {
+    var x = d[""];
+    delete d[""];
+    for (let prop in d) {
+      var y = prop,
+        value = d[prop];
+      data.push({
+        x: x,
+        y: y,
+        value: +value
+      });
+    }
+  });
+
+  var margin = {
+      top: 25,
+      right: 80,
+      bottom: 25,
+      left: 25
+    },
+    width = 500 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom,
+    domain = d3
+      .set(
+        data.map(function(d) {
+          return d.x;
+        })
+      )
+      .values(),
+    num = Math.sqrt(data.length),
+    color = d3
+      .scaleLinear()
+      .domain([-1, 0, 1])
+      .range(["#B22222", "#fff", "#000080"]);
+
+  var x = d3
+      .scalePoint()
+      .range([0, width])
+      .domain(domain),
+    y = d3
+      .scalePoint()
+      .range([0, height])
+      .domain(domain),
+    xSpace = x.range()[1] - x.range()[0],
+    ySpace = y.range()[1] - y.range()[0];
+  ySpace = y.range()[1] - y.range()[0];
+
+  var svg = d3
+    .select("#matrix")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var cor = svg
+    .selectAll(".cor")
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("class", "cor")
+    .attr("transform", function(d) {
+      return "translate(" + x(d.x) + "," + y(d.y) + ")";
+    });
+
+  // it fits with v3
+  // cor.append("rect")
+  //   .attr("width", xSpace)
+  //   .attr("height", ySpace)
+  //   .attr("x", -xSpace / 2)
+  //   .attr("y", -ySpace / 2)
+
+  //edited to fit with v4 update  2/5/18
+  cor
+    .append("rect")
+    .attr("width", xSpace / 10)
+    .attr("height", ySpace / 10)
+    .attr("x", -xSpace / 20)
+    .attr("y", -ySpace / 20);
+
+  cor
+    .filter(function(d) {
+      var ypos = domain.indexOf(d.y);
+      var xpos = domain.indexOf(d.x);
+      for (var i = ypos + 1; i < num; i++) {
+        if (i === xpos) return false;
+      }
+      return true;
+    })
+    .append("text")
+    .attr("y", 5)
+    .text(function(d) {
+      if (d.x === d.y) {
+        return d.x;
+      } else {
+        return d.value.toFixed(2);
+      }
+    })
+    .style("fill", function(d) {
+      if (d.value === 1) {
+        return "#000";
+      } else {
+        return color(d.value);
+      }
+    });
+
+  cor
+    .filter(function(d) {
+      var ypos = domain.indexOf(d.y);
+      var xpos = domain.indexOf(d.x);
+      for (var i = ypos + 1; i < num; i++) {
+        if (i === xpos) return true;
+      }
+      return false;
+    })
+    .append("circle")
+    .attr("r", function(d) {
+      return (width / (num * 2)) * (Math.abs(d.value) + 0.1);
+    })
+    .style("fill", function(d) {
+      if (d.value === 1) {
+        return "#000";
+      } else {
+        return color(d.value);
+      }
+    });
+
+  var aS = d3
+    .scaleLinear()
+    .range([-margin.top + 5, height + margin.bottom - 5])
+    .domain([1, -1]);
+
+  var yA = d3
+    .axisRight()
+    .scale(aS)
+    .tickPadding(7);
+
+  var aG = svg
+    .append("g")
+    .attr("class", "y axis")
+    .call(yA)
+    .attr("transform", "translate(" + (width + margin.right / 2) + " ,0)");
+
+  var iR = d3.range(-1, 1.01, 0.01);
+  var h = height / iR.length + 3;
+  iR.forEach(function(d) {
+    aG.append("rect")
+      .style("fill", color(d))
+      .style("stroke-width", 0)
+      .style("stoke", "none")
+      .attr("height", h)
+      .attr("width", 10)
+      .attr("x", 0)
+      .attr("y", aS(d));
+  });
+};
+
+const getCorMatrixData = (codeList, startDate, endDate) => {
+  const listOfPriceList = codeList.map(code =>
+    market.getReturnsListInRange(code, startDate, endDate)
+  );
+
+  const corList = [];
+  for (let i = 0; i < codeList.length; i++) {
+    let row = {};
+    for (let j = 0; j < codeList.length; j++) {
+      const corrcoeff = jStat.corrcoeff(listOfPriceList[i], listOfPriceList[j]);
+      row[codeList[j]] = corrcoeff;
+    }
+    corList.push(row);
+  }
+
+  corList["columns"] = [""].concat(codeList);
+
+  console.log(corList);
+  return corList;
+};
 
 class Test extends React.Component {
   render() {
@@ -210,26 +233,8 @@ class Test extends React.Component {
             "130730"
           ];
 
-          const listOfPriceList = codeList.map(code =>
-            market.getReturnsListInRange(code, startDate, endDate)
-          );
-
-          console.log(listOfPriceList);
-
-          const corList = [];
-          for (let i = 0; i < codeList.length; i++) {
-            let subCorList = [];
-            for (let j = 0; j < codeList.length; j++) {
-              const corrcoeff = jStat.corrcoeff(
-                listOfPriceList[i],
-                listOfPriceList[j]
-              );
-              subCorList.push(corrcoeff);
-            }
-            corList.push(subCorList);
-          }
-
-          console.log(corList);
+          const result = getCorMatrixData(codeList, startDate, endDate);
+          console.log(result);
           return null;
         }}
       </Query>
