a04f33cb42e1919fda41b309824b9518c964a336
woo jae <dnwogo@naver.com>
2019-05-26 05:41:05 +0900

    add arg type

--- src/pages/BackTestPage/StrategyArgSelect.js
+++ src/pages/BackTestPage/StrategyArgSelect.js
@@ -18,6 +18,9 @@ class StrategyArgSelect extends React.Component {
         <Option value="4">4</Option>
         <Option value="5">5</Option>
         <Option value="6">6</Option>
+        <Option value="7">7</Option>
+        <Option value="8">8</Option>
+        <Option value="9">9</Option>
         <Option value="10">10</Option>
         <Option value="20">20</Option>
         <Option value="30">30</Option>
