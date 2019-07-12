366301f838376ef8e5cc30234dce2073ab9f5734
woo jae <dnwogo@naver.com>
2019-07-12 23:38:11 +0900

    apply react-grid-layout test

--- src/pages/BackTestPage/BackTestPresenter.js
+++ src/pages/BackTestPage/BackTestPresenter.js
@@ -6,6 +6,7 @@ import PortFolioPositionChart from "./PortFolioPositionChart";
 import StrategyDescription from "./StrategyDescription";
 import { Switch } from "antd";
 import { Button } from "antd";
+import GridLayout from "react-grid-layout";
 
 const BackTestPresenter = props => {
   const {
@@ -26,24 +27,30 @@ const BackTestPresenter = props => {
   const { globalVariables } = data;
   const { runSimulation } = func;
 
+  var layout = [
+    { i: "a", x: 0, y: 0, w: 12, h: 4, static: true },
+    { i: "b", x: 0, y: 4, w: 12, h: 12, static: true },
+    { i: "c", x: 0, y: 16, w: 7, h: 15, static: true },
+    { i: "d", x: 7, y: 16, w: 5, h: 15, static: true },
+    { i: "e", x: 0, y: 31, w: 12, h: 10, static: true }
+  ];
+
   return (
-    <div>
-      <StrategyDescription />
-      <WeightsInputTable
-        columns={columns}
-        dataSource={dataSource}
-        runHandler={(
-          weightsList,
-          name,
-          rebalanceType,
-          strategyType,
-          strategyArg1,
-          strategyArg2,
-          strategyArg3,
-          selectedAsset
-        ) =>
-          runSimulation(
-            globalVariables,
+    <GridLayout
+      className="layout"
+      layout={layout}
+      cols={12}
+      rowHeight={30}
+      width={1600}
+    >
+      <div key="a">
+        <StrategyDescription />
+      </div>
+      <div key="b">
+        <WeightsInputTable
+          columns={columns}
+          dataSource={dataSource}
+          runHandler={(
             weightsList,
             name,
             rebalanceType,
@@ -52,28 +59,46 @@ const BackTestPresenter = props => {
             strategyArg2,
             strategyArg3,
             selectedAsset
-          )
-        }
-        rootComp={rootComp}
-        batchSelection={batchSelection}
-      />
-      <Button type="default" onClick={() => refreshHandler(globalVariables)}>
-        Refresh
-      </Button>
-      <PriceChart
-        data={data}
-        resultList={resultList}
-        selectPortfolio={selectPortfolioHandler}
-        isLogScale={isLogScale}
-      />
-      <span>Returns Log scale </span>
-      <Switch onChange={setLogScale} />
-      <BackTestResultTable data={resultList} />
-      <PortFolioPositionChart
-        resultList={resultList}
-        portInfo={selectedPortfolio}
-      />
-    </div>
+          ) =>
+            runSimulation(
+              globalVariables,
+              weightsList,
+              name,
+              rebalanceType,
+              strategyType,
+              strategyArg1,
+              strategyArg2,
+              strategyArg3,
+              selectedAsset
+            )
+          }
+          rootComp={rootComp}
+          batchSelection={batchSelection}
+        />
+        <Button type="default" onClick={() => refreshHandler(globalVariables)}>
+          Refresh
+        </Button>
+      </div>
+      <div key="c">
+        <PriceChart
+          data={data}
+          resultList={resultList}
+          selectPortfolio={selectPortfolioHandler}
+          isLogScale={isLogScale}
+        />
+        {/* <span>Returns Log scale </span>
+        <Switch onChange={setLogScale} /> */}
+      </div>
+      <div key="d">
+        <PortFolioPositionChart
+          resultList={resultList}
+          portInfo={selectedPortfolio}
+        />
+      </div>
+      <div key="e">
+        <BackTestResultTable data={resultList} />
+      </div>
+    </GridLayout>
   );
 };
 
