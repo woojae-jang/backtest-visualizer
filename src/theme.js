8834bf773a724c1f3ffe05ea0828c5ae2a256bdc
woo jae <dnwogo@naver.com>
2019-07-18 12:52:09 +0900

    .

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,13 @@
-const theme = {
-  mainColor: "#2B353A",
-  textColor: "#FFFFFF",
-  subText1Color: "#4D83D7",
-  subText2Color: "#00009F",
-  subText3Color: "#1050AE",
-  bullColor: "#00B909",
-  bearColor: "#C60606"
-};
+// const theme = {
+//   mainColor: "#2B353A",
+//   textColor: "#FFFFFF",
+//   subText1Color: "#4D83D7",
+//   subText2Color: "#00009F",
+//   subText3Color: "#1050AE",
+//   bullColor: "#00B909",
+//   bearColor: "#C60606"
+// };
 
-// const theme = {};
+const theme = {};
 
 export default theme;
