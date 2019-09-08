cdb69b93b6a9f138a65ad17144a926a2e4805cc3
woo jae <dnwogo@naver.com>
2019-09-08 14:16:10 +0900

    Updates

---
+++ precache-manifest.8bf81da7dac00ff637b19ed8ff0f7649.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbfe5210d02da56b733e",
+    "url": "/github.io/backtest-visualizer/static/js/runtime~main.dbfe5210.js"
+  },
+  {
+    "revision": "a038d5b0f6a4736318ef",
+    "url": "/github.io/backtest-visualizer/static/js/main.a038d5b0.chunk.js"
+  },
+  {
+    "revision": "a30cd48dc52bfad32474",
+    "url": "/github.io/backtest-visualizer/static/js/2.a30cd48d.chunk.js"
+  },
+  {
+    "revision": "a038d5b0f6a4736318ef",
+    "url": "/github.io/backtest-visualizer/static/css/main.660a598c.chunk.css"
+  },
+  {
+    "revision": "a30cd48dc52bfad32474",
+    "url": "/github.io/backtest-visualizer/static/css/2.ceab03f6.chunk.css"
+  },
+  {
+    "revision": "f68d9b9d6ddaaa49300663f1357da147",
+    "url": "/github.io/backtest-visualizer/index.html"
+  }
+];
